29bdef9e0bf7db545e9402d8d837fef70c7b796b
Indra Lukmana <indralukmana@live.com>
2020-06-16 17:02:33 +0700

    🚀 RELEASE: Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+const Example = {
+  name: "next",
+};
+
+export default function Home() {
+  return (
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <h1>
+        <p>{Example.name}</p>
+      </h1>
+    </div>
+  );
+}
