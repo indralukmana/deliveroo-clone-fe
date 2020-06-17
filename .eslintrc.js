f8cd42f8f5e0de9f43aec10ef5d10d28e438f5a0
Indra Lukmana <indralukmana@live.com>
2020-06-17 08:24:19 +0700

    📦 NEW: Add Material-UI package

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,21 @@
 module.exports = {
-  extends: ["./node_modules/poetic/config/eslint/eslint-config.js"],
+  extends: ['./node_modules/poetic/config/eslint/eslint-config.js'],
   // Add custom rules here
-  rules: {}
+  plugins: ['import'],
+  rules: {
+    'no-unused-expressions': 'off', // https://github.com/eslint/eslint/issues/12822
+    'react/jsx-props-no-spreading': [
+      'warn',
+      {
+        exceptions: ['App', 'Component'],
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        directory: '.',
+      },
+    },
+  },
 };
