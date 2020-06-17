292362ac971ecd45995eb0b6fe293b2ea94304f6
Indra Lukmana <indralukmana@live.com>
2020-06-17 10:59:04 +0700

    📦 NEW: Setup twin.macro for styling using tailwind

--- src/layout/main.tsx
+++ src/layout/main.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
+import tw from 'twin.macro';
 
 const MainLayout = ({ children }: LayoutProps): JSX.Element => {
-  return <div>{children}</div>;
+  return <div css={[tw`min-h-screen bg-gray-300`]}>{children}</div>;
 };
 
 export default MainLayout;
