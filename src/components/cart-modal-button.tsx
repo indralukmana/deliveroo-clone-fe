b27a71be63f3c21693ee374484656b757b9417cb
Indra Lukmana <indralukmana@live.com>
2020-06-23 08:07:04 +0700

    📦 NEW: Add cart modal structure base

--- src/components/cart-modal-button.tsx
+++ src/components/cart-modal-button.tsx
@@ -1,10 +1,13 @@
 import { Backdrop, Fade, IconButton, Modal, Paper } from '@material-ui/core';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import { AddCircle, RemoveCircle } from '@material-ui/icons';
 import React, { useState } from 'react';
 import tw from 'twin.macro';
+import { useCart, ActionType } from 'src/Context/Cart';
 
 const CartModalButton = (): JSX.Element => {
   const [open, setOpen] = useState(false);
+  const { cart, cartDispatch } = useCart();
 
   const handleClose = (): void => {
     setOpen(false);
@@ -32,8 +35,30 @@ const CartModalButton = (): JSX.Element => {
         css={[tw`flex justify-center items-center`]}
       >
         <Fade in={open}>
-          <Paper>
+          <Paper css={[tw`p-5`]}>
             <h2 id="transition-modal-title">Cart</h2>
+            {cart.dishOrders.map((dish) => (
+              <div css={[tw`flex justify-between items-center`]}>
+                <span>{dish.name}</span>
+                <div>
+                  <IconButton
+                    onClick={(): void =>
+                      cartDispatch({ type: ActionType.AddToCart, payload: dish })
+                    }
+                  >
+                    <AddCircle />
+                  </IconButton>
+                  <span>{dish.count}</span>
+                  <IconButton
+                    onClick={(): void =>
+                      cartDispatch({ type: ActionType.RemoveFromCart, payload: dish })
+                    }
+                  >
+                    <RemoveCircle />
+                  </IconButton>
+                </div>
+              </div>
+            ))}
           </Paper>
         </Fade>
       </Modal>
